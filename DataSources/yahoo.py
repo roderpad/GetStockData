074b9f522c912bd68af9ff2fc2d489393ecfeec7
Paden B. Roder <roderpad@gmail.com>
2021-08-03 20:00:10 -0700

    Complete yahoo datapull example

--- DataSources/yahoo.py
+++ DataSources/yahoo.py
@@ -1,7 +1,15 @@
 import yfinance as yf
 
-slack = yf.Ticker("WORK")
+slack = yf.Ticker("WORK") # WORK is the ticker for Slack
 
-history = slack.history(period="max")
+history = slack.history(period="max") # Get the slack stock price history with max period
 
-print(history)
+print('The output type is:')
+print(type(history)) # Prints the type that history outputs
+print('')
+print('The history output is the following:')
+print(history) # Prints history
+print('')
+print('The csv output of history is the following:')
+print(history.to_csv()) # Prints history as .csv file.
+# To create [filename].csv for this output, run 'python DataSources/yahoo.py > [filename].csv
